1c531754d0e133c615476741173c11ee8d588441
moonbastard <120229608+moonbastard@users.noreply.github.com>
2023-03-15 13:50:15 -0700

    Update URLanalyzer.py

--- URLanalyzer.py
+++ URLanalyzer.py
@@ -7,42 +7,33 @@ def get_mentions_count(content, phrase):
     lower_phrase = phrase.lower()
     return lower_content.count(lower_phrase)
 
-# Input source URL
-source_url = input("Enter source URL: ")
-source_page = requests.get(source_url)
-source_content = source_page.text
-
-# Get the word count of source URL
-source_word_count = len(source_content.split())
-print(f"Source URL word count: {source_word_count}")
-
-# Input search phrase
-phrase = input("Enter a phrase to search for: ")
-
-# Get count of mentions of the phrase in source URL
-source_mentions = get_mentions_count(source_content, phrase)
-print(f"Number of mentions in source URL: {source_mentions}")
-
-# Input target URLs
-target_urls = [input("Enter target URL: ") for i in range(3)]
-
-# Get count of mentions of the phrase in target URLs
-for url in target_urls:
-    target_page = requests.get(url)
-    target_content = target_page.text
-
-    # Get the word count of target URL
-    target_word_count = len(target_content.split())
-    print(f"Target URL word count: {target_word_count}")
-
-    target_mentions = get_mentions_count(target_content, phrase)
-    print(f"Number of mentions in target URL: {target_mentions}")
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/result', methods=['POST'])
+def result():
+    source_url = request.form['source_url']
+    phrase = request.form['phrase']
+    target_urls = [
+        request.form['target_url_1'],
+        request.form['target_url_2'],
+        request.form['target_url_3'],
+    ]
+
+    source_page = requests.get(source_url)
+    source_content = source_page.text
+    source_word_count = len(source_content.split())
+    source_mentions = get_mentions_count(source_content, phrase)
+
+    target_results = []
+    for url in target_urls:
+        target_page = requests.get(url)
+        target_content = target_page.text
+        target_word_count = len(target_content.split())
+        target_mentions = get_mentions_count(target_content, phrase)
+        target_results.append((url, target_word_count, target_mentions))
+
+    return render_template('result.html', source_url=source_url, source
